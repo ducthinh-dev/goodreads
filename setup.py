5667c8bdce1e8389eda10976b4483146e2b8d13b
alice <dothinh.dev@gmail.com>
2023-06-09 11:00:11 +0700

    splitted train test data

---
+++ setup.py
@@ -0,0 +1,28 @@
+def setup():
+    import sqlalchemy as sa
+    import mysql.connector
+    import getpass
+    
+    HOST = 'localhost'
+    USER = 'root'
+    DATABASE = 'goodreads'
+    PASSWORD = getpass.getpass(f'Enter password for {USER}: ')
+
+    def getconn():
+        conn = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+        return conn
+
+    pool = sa.create_engine(
+        "mysql+mysqlconnector://",
+        creator=getconn,
+    )
+
+    with pool.connect() as db_conn:
+        results = db_conn.execute(sa.text("SELECT NOW()")).fetchone()
+        print("Current time: ", results[0])
+    return pool
