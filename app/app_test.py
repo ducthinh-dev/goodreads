363bbcf526a2b98a8442145e67c81a1e0b6e6faa
alice <dothinh.dev@gmail.com>
2023-06-13 13:08:16 +0700

    added tfidf model to book page and user page

--- app/app_test.py
+++ app/app_test.py
@@ -1,51 +1,9 @@
-# first_time = True
-# if first_time:
-#     import sys
-#     from os import path
-#     dir = path.abspath('__file__')
-#     sys.path.append(path.dirname(dir))
-#     import streamlit as st
-#     import pandas as pd
-#     import numpy as np
-#     import sqlalchemy as sa
-#     import mysql.connector
-
-#     from setup import setup
-#     # from models.doc2vec import Doc2VecModel
-#     from models.tfidf import TfidfModel
-#     connector = setup()
-#     books_query = sa.text(
-#         'select * from books;'
-#     )
-#     books = pd.read_sql_query(books_query, con=connector.connect())
-
-#     descr_query = sa.text(
-#         'select * from processed_description;'
-#     )
-#     descr = pd.read_sql_query(descr_query, con=connector.connect())
-#     ratings = pd.read_csv('./data/main/ratings_train.dat', sep=':', header=None)
-#     ratings_test = pd.read_csv('./data/main/ratings_test.dat', sep=':', header=None)
-#     st.title('DEMO APP')
-#     model = TfidfModel(descr, 'goodreads_book_id', 'processed_descr')
-#     model.fit()
-#     indices = pd.Series(books.index,index=books['goodreads_book_id'].astype(int))
-#     first_time = False
-
-# with st.form('id'):
-#     input_id = st.text_input('Enter book id')
-#     no_recommends = st.number_input('Enter number of recommends', min_value=2, max_value=100, step=1)
-#     submit_button = st.form_submit_button('Submit')
-
-# if submit_button:
-#     recommends = model.get_recommendations(int(input_id), no_recommends)
-#     chosen_book = books.loc[books.goodreads_book_id == input_id]
-#     st.write(f'recommends for {chosen_book.title.values[0]}')
-#     recommended_book = books[['title', 'average_rating', 'description']].iloc[indices[recommends]]
-#     recommended_book.reset_index(inplace=True)
-#     recommended_book.drop(columns=['index'], inplace=True)
-#     st.write(recommended_book)
-
-
+import joblib
+import requests
+from PIL import Image
+import time
+from models.tfidf import TfidfModel
+from setup import setup
 import mysql.connector
 import sqlalchemy as sa
 import numpy as np
@@ -56,88 +14,110 @@ from os import path
 dir = path.abspath('__file__')
 sys.path.append(path.dirname(dir))
 
-from setup import setup
-from models.tfidf import TfidfModel
-import time
-from PIL import Image
-import requests
-
-import joblib
 
 st.set_page_config(
     page_title='Books recommender',
-    page_icon = '📚'
+    page_icon='📚'
 )
-def get_rec(model):
+
+
+def get_recommendations(model, top):
+    return model
+
+
+def book_page():
+    st.title('Books')
+    get_data(book=True)
+    change_model = st.button('Change model')
+    if change_model:
+        st.session_state["model"] = get_model(model_type='doc2vec')
+        st.session_state["model_type"] = 'doc2vec'
+    st.write('MODEL: ', st.session_state.model_type.upper())
+
     with st.form('id'):
         input_id = st.text_input('Enter book id')
-        no_recommends = st.number_input('Enter number of recommends', min_value=2, max_value=100, step=1)
+        no_recommends = st.number_input(
+            'Enter number of recommends', min_value=2, max_value=100, step=1)
         submit_button = st.form_submit_button('Submit')
-
     if submit_button:
-        recommends = model.get_recommendations(int(input_id), no_recommends)
+        books = st.session_state.books
+        indices = st.session_state.indices
+        recommends = st.session_state.model.get_recommendations(
+            int(input_id), no_recommends)
         chosen_book = books.loc[books.goodreads_book_id == input_id]
         st.write(f'recommends for {chosen_book.title.values[0]}')
-        recommended_book = books[['title', 'average_rating', 'description']].iloc[indices[recommends]]
+        recommended_book = books[[
+            'title', 'average_rating', 'description']].iloc[indices[recommends]]
         recommended_book.reset_index(inplace=True)
         recommended_book.drop(columns=['index'], inplace=True)
         st.write(recommended_book)
 
-def book_page():
-    st.title('Books')
-    data_btn = st.button('Get data')
-    model_btn = st.button('Get model')
-    if data_btn:
-        get_data(book=True)
-        success = st.success('Books data ready!', icon='📚')
-        time.sleep(3)
-        success.empty()
-    if model_btn:
-        model = get_model()
-        success = st.success('Model ready!', icon='🤖')
-        time.sleep(3)
-        success.empty()
-        get_rec(model)
-    # if st.button('Get image'):
-        # url = r'https://picsum.photos/200/400'
-        # img = st.image(Image.open(requests.get(url, stream=True).raw))
-        
+
 def user_page():
     st.title('Users')
-    if st.button('Get train data'):
-        get_data(user=True)
-        success = st.success('Train data ready!', icon='🧑‍🏫')
-        time.sleep(3)
-        success.empty()
-        
-    if st.button('Get test data'):
-        success = st.success('Test data ready!', icon='🧑‍🏫')
-        time.sleep(3)
-        success.empty()
+    get_data(user=True, test=True)
+    books = st.session_state.books
+    indices = st.session_state.indices
+    users = st.session_state.users
+    users_test = st.session_state.test
+    change_model = st.button('Change model')
+    if change_model:
+        st.session_state.model = get_model(model_type='doc2vec')
+        st.session_state.model_type = 'doc2vec'
+    st.write('MODEL: ', st.session_state.model_type.upper())
+
+    with st.form('id'):
+        input_id = st.text_input('Enter user id')
+        no_recommends = st.number_input(
+            'Enter number of recommends', min_value=2, max_value=100, step=1)
+        submit_button = st.form_submit_button('Submit')
+    if submit_button:
+        user_profile = users.loc[users[0] == int(input_id)]
+        uitems = user_profile[1].astype(int).tolist()
+        uratings = user_profile[2].tolist()
+        st.session_state.model.update_user(uitems, uratings, 100)
+        recommends = st.session_state.model.get_personal_recommendations(no_recommends)
+        recommended_book = books[[
+            'title', 'average_rating', 'description']].iloc[indices[recommends]]
+        recommended_book.reset_index(inplace=True)
+        recommended_book.drop(columns=['index'], inplace=True)
+        st.write(recommended_book)
+
 
 pages = {
     'By book': book_page,
     'By user': user_page
 }
 
+
 def get_data(book=False, user=False, test=False):
-    global books, indices, users, users_test
     if book:
         connector = setup()
         query = sa.text(
             'select * from books;'
         )
         books = pd.read_sql_query(query, con=connector.connect())
-        indices = pd.Series(books.index,index=books['goodreads_book_id'].astype(int))
+        indices = pd.Series(
+            books.index, index=books['goodreads_book_id'].astype(int))
+        st.session_state.books = books
+        st.session_state.indices = indices
     if user:
-        users = pd.read_csv(r'D:/project/goodreads/data/main/ratings_train.dat', sep=':', header=None)
+        users = pd.read_csv(
+            r'D:/project/goodreads/data/main/ratings_train.dat', sep=':', header=None)
+        st.session_state.users = users
     if test:
-        users_test = pd.read_csv(r'D:/project/goodreads/data/main/ratings_test.dat', sep=':', header=None)
+        users_test = pd.read_csv(
+            r'D:/project/goodreads/data/main/ratings_test.dat', sep=':', header=None)
+        st.session_state.test = users_test
+
 
 def get_model(model_type='tfidf'):
     if model_type == 'tfidf':
         model = joblib.load(r'D:\project\goodreads\dumbed_models\tfidf.joblib')
+    if model_type == 'doc2vec':
+        model = 'doc2vec'
     return model
+
 
 def main():
     st.sidebar.title('')
@@ -146,4 +126,6 @@ def main():
 
 
 if __name__ == '__main__':
+    st.session_state.model = get_model()
+    st.session_state.model_type = 'tfidf'
     main()
